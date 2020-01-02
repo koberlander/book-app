45ea9a077c8a56f7e0230a11108d7e7a4105d93d
Kristin M. Oberlander <koberlander115@gmail.com>
2020-01-02 15:37:46 -0500

    Add get books and books id routes

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World...boop')
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.get('/api/books', (req, res) => {
+  res.send([1, 2, 3, 'boop'])
+})
+
+app.get('/api/books/:id', (req, res) => {
+  res.send(req.params.id)
+})
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
