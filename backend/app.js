5633d67446717522a2454b2e1599f04dc56b090c
Kristin M. Oberlander <koberlander115@gmail.com>
2020-01-02 17:25:40 -0500

    Add post route and data validation via Joi

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,83 @@
-
+const Joi = require('joi');
 const express = require('express');
 const app = express()
 
+app.use(express.json())
+
+const books = [
+   {
+      "id":    1,
+      "title": "Business"
+   },
+
+   {
+      "id":    2,
+      "title": "History"
+   },
+   {
+      "id":    3,
+      "title": "Computers"
+   },
+   {
+      "id":    4,
+      "title": "Science"
+   },
+   {
+      "id":    5,
+      "title": "Fiction"
+   },
+   {
+      "id":    6,
+      "title": "Travel"
+   }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World...boop')
+  res.send(books)
 })
 
 app.get('/api/books', (req, res) => {
-  res.send([1, 2, 3, 'boop'])
+  res.send(books)
 })
 
+app.post('/api/books', (req, res) => {
+  const schema = {
+    genre: Joi.string().min(3)
+  }
+
+  const result = Joi.validate(req.body, schema)
+
+  if(result.error){
+    res.status(400).send(res.error.details[0].message)
+    return
+  }
+
+  const book = {
+    genre: req.body.genre
+  }
+  books.push(book)
+  res.send(book)
+})
+
+app.put('/api/books/:id', (req, res) => {
+  // Find the book
+  // If no book, return 404
+
+  // Validate
+  // If invalid, return 400 - Bad request
+
+  // Update the book
+  // Return the updated book
+})
+
+
+
 app.get('/api/books/:id', (req, res) => {
-  res.send(req.params.id)
+  const book = books.find(b => b.id === req.params.id)
+
+  if (!book) res.status(404).send('Book with that ID not found.')
+
+  res.send(book)
 })
 
 // PORT
